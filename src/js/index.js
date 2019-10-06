685e1cd4be3135130eda4a312b7a968c9bf11e4c
Rubén Zagon <ruben.zagon@gmail.com>
2019-10-06 22:30:19 +0100

    Chance ALL actual code in javascript. Now working with objects
     On branch refactor
     Changes to be committed:
    	modified:   src/js/index.js

--- src/js/index.js
+++ src/js/index.js
@@ -16,112 +16,38 @@ A = 1/11 (Si estoy por menos de 10 vale 11 y si estas por más de 10 entonces va
 // Generar baraja
 // ['1C', '5P', '7T' .....]
 
-const sticks = ["C", "P", "R", "T"];
 
-const generateRange = () => {
-  let range = ['A', 'J', 'Q', 'K'];
 
-  for (var i = 2; i <= 10; i++) {
-    range.push(i);
+const deck = () => {
+    const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    const sticks = ["C", "P", "R", "T"]; 
+    let deck = [];
+  
+    this.getDeck = () =>{
+      return this.setDeck();
+    } 
+  
+    this.setDeck = () => {
+      for (i = 0; i < ranks.length; i++) {
+        for (j = 0; j < sticks.length; j++){
+          
+          let card = new Card({ //Declaramos el objeto carta
+            'rank': ranks[i]
+          });
+  
+          deck.push({ // Le definimos a cada carta un rango, un palo y un valor númerico para luego procesorlo en la puntuación.
+            'rank': ranks[i],
+            'stick': sticks[j],
+            'value': card.getValue()
+  
+          });
+        }
+  
+       return deck;
+      }
+    };
+  
   }
-
-  return range;
-}
-
-// console.log (generateRange()); <---------------------------------------  UNCOMMENT
-
-const generateDeck = (sticks) => {
-  let deck = [];
-  let range = generateRange();
-
-  for (var i = 0; i < sticks.length; i++) {
-    for (var j = 0; j < range.length; j++) {
-      let card = range[j] + sticks[i];
-      deck.push(card);
-    }
-  }  
-
-  return deck;
-}
-
-
-const generateRandomDeck = (deck) => {
-    let randomPosition, saveElementInActualPosition, positionActual;
-    for (positionActual = deck.length - 1; positionActual > 0; positionActual--) {
-
-        randomPosition = Math.floor(Math.random() * (positionActual + 1));
-        saveElementInActualPosition = deck[positionActual];
-        deck[positionActual] = deck[randomPosition];
-        deck[randomPosition] = saveElementInActualPosition;
-
-    }
-    return deck;
-}
-
-let deckForPlay = generateRandomDeck(generateDeck(sticks));
-
-
-// console.log(deckForPlay);   <---------------------------------------  UNCOMMENT
-
-
-// Repartimos cartas
-// - Son 2 cartas al jugador y 1 al crupier al inicar una partida.
-let cardsPlayer = []
-let cardsCrupier = [];
-
-const getCardOfDeck = (deck) => {
-   return deck.pop();
-}
-
-const firstRound = (jugador, crupier) => {
-    jugador.push(getCardOfDeck(deckForPlay));
-    jugador.push(getCardOfDeck(deckForPlay));
-
-    crupier.push(getCardOfDeck(deckForPlay));
-    return;
-}
-
-firstRound(cardsPlayer, cardsCrupier); // Repardo 2 cartas al jugador y 1 al crupier
-
-let colorPlayer = '#ffac33';
-let colorCrupier = '#d1ff33';
-console.log(`%cCartas del jugador: ${cardsPlayer}`,'background:' + colorPlayer);
-console.log(`%cCartas del crupier: ${cardsCrupier}`, 'background:' + colorCrupier);
-console.log(`Tamaño de la baraja actual: ${deckForPlay.length}`);
-
-// #############################################  CALCULO DE PUNTUACIÓN
-/**
- * 
- * Aquí me funciona ahora mismo el que me sume el contenido del Array si tiene números, pero ahora tengo que hacer que cuando lea 'J', 'Q', 'K' los tome como 10 de valor.
- * 
- */
-
-let pointsPlayer = 0;
-
-let separar = (array) =>{
-  //recorrer el array o que nos de el elemento index primero del string
-  let conjunto = [];
-
-  for (i = 0; i < cardsPlayer.length ; i++) {
-
-    let numb = parseInt(cardsPlayer[i].substr(0, 1));
-      conjunto.push(numb);
-  }
-  return conjunto
-}
-
-let sumarArray = (arrayASumar) => {
-  let add = 0;
-
-  for (i = 0 ; i < arrayASumar.length; i++) {
-    add = add + arrayASumar[i];
-  }
-  return add;
-}
-
-pointsPlayer = sumarArray(separar(cardsPlayer));
-
-console.log(pointsPlayer);
-
-// #################################################   
-
+  
+  console.log(deck());
+  
