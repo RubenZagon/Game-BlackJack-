b0d66079eca10208246171ac3540db05bff3b0ba
Ruben Zagon <ruben.zagon@gmail.com>
2019-10-12 16:48:21 +0100

    First round OK:raising_hand_man:

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,81 @@
-// REGLAS DEL JUEGO
-// http://www.casino.es/blackjack/como-jugar-blackjack/
-
-//Los comentarios para hacer el seguimiento de los procesos están diseñados principalmente para usar la consola de Chrome
-
-// Crupier y jugador
-// Se reparten 2 cartas para el jugador y una para el crupier, si no llega a 21 le preguntas al jugador si quiere seguir o se planta. Y tu como crupier hacer los mismo.
-/*
-- generar cartas (2 - 9 - J Q K A) / barajar
-- Barajar
-- Repartir cartas
-- comprobar Puntuación
-A = 1/11 (Si estoy por menos de 10 vale 11 y si estas por más de 10 entonces vale 1)
-*/
-
-// Generar baraja
-// ['1C', '5P', '7T' .....]
-
 import { Deck } from './Elements/deck.js';
 import { Player } from './Elements/players.js';
 
 
 
+let deck = new Deck(),
+gameDeck = deck.shuffle()
 
 let player = new Player(),
     crupier = new Player()
 
-  let deck = new Deck(),
-    gameDeck = deck.shuffle()
-
-
-
 //Comienzo del juego
-player.pickCard(gameDeck)
-player.pickCard(gameDeck)
 
-crupier.pickCard(gameDeck)
+const firstRound = () => {
 
-console.log('Player')
-console.table(player.hand)
-console.log('Crupier')
-console.table(crupier.hand)
+  player.pickCard(gameDeck)
+  player.pickCard(gameDeck)
 
-console.log(`Tamaño de la baraja: ${gameDeck.length}`)
+  crupier.pickCard(gameDeck)
+}
+
+
+//Valorar puntuación
+const watchPunctuation = (playerObj) => {
+  let points = 0
+
+  for (let i = 0; i < playerObj.hand.length; i++){
+    points += playerObj.hand[i].value
+  }
+  return points
+}
+
+//Preguntar al jugador si continuar o no
+const askPlayer = () => {
+  let answer = 'y' //prompt('¿Pedir carta?: [y/n]', 'y')
+  console.log(`Respuesta del jugador: ${answer}`)
+  if (answer === 'y'){player.pickCard(gameDeck)}
+}
+
+// ######   FUNCION DE SEGUIMIENTO    ########
+const mostrarPuntuaciones = () => {
+
+  console.log('Player')
+  console.table(player.hand)
+  console.log('Crupier')
+  console.table(crupier.hand)
+  
+  console.log(`Tamaño de la baraja: ${gameDeck.length}`)
+  
+  // Mostrar puntuación de las cartas
+  console.log (`Puntuacion del jugador: ${pointsPlayer}`)
+  console.log (`Puntuacion del crupier: ${pointsCrupier}`)
+
+}
+
+const whoIsTheWinner = () => {
+  
+}
+
+
+firstRound()
+
+//test
+mostrarPuntuaciones()
+
+let pointsPlayer = watchPunctuation(player),
+    pointsCrupier = watchPunctuation(crupier)
+
+askPlayer()
+
+//test despues de preguntar
+mostrarPuntuaciones()
+
+
+
+
+
+  //Si jugador continua, dar otra carta y valorar puntuación
 
 
 
